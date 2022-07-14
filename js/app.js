27fac5854c7388f316452cc19f1c018981776542
dms233 <diogo.c.sousa@itsector.pt>
2022-07-14 10:04:29 +0100

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,7 @@
+var vendingMachine = (function () {
+  function vendingMachine() {}
+  return vendingMachine;
+})();
+// /// <reference path="vendingMachine.ts" />
+var machine = new vendingMachine();
+//# sourceMappingURL=app.js.map
